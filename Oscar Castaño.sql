f5cb469daf9e1a863142b5b8e865ec7352559bb8
Oscar Felipe Castaño Martinez <40299728+osf985elcas@users.noreply.github.com>
2018-10-09 11:57:35 -0500

    Update Oscar Castaño.sql

--- Oscar Castaño.sql
+++ Oscar Castaño.sql
@@ -2,7 +2,7 @@ CREATE TABLE REGIONS(
 region_id int(5) NOT NULL,
 region_name varchar(30), 
 PRIMARY KEY(region_id)
-)
+);
 
 CREATE TABLE COUNTRIES(
 country_id int(5) NOT NULL,
@@ -11,7 +11,7 @@ region_id int (5) NOT NULL,
 PRIMARY KEY(country_id),
 CONSTRAINT FK_region_id FOREIGN KEY(region_id) REFERENCES REGIONS(region_id)
 ON UPDATE CASCADE
-)
+);
 CREATE TABLE LOCATIONS(
 location_id int(5) NOT NULL,
 street_address varchar(30), 
@@ -22,7 +22,7 @@ country_id int(5) NOT NULL ,
 PRIMARY KEY(location_id),
 CONSTRAINT FK_country_id FOREIGN KEY(country_id) REFERENCES COUNTRIES(country_id)
 ON UPDATE CASCADE
-)
+);
 CREATE TABLE DEPARTAMENTS(
 departament_id int(5) NOT NULL,
 departament_name varchar(30) NOT NULL,
@@ -33,12 +33,12 @@ CONSTRAINT FK_location_id FOREIGN KEY(location_id) REFERENCES LOCATIONS(location
 ON UPDATE CASCADE,
 CONSTRAINT FK_manager_id FOREIGN KEY(manager_id) REFERENCES MANAGER(manager_id) 
 ON UPDATE CASCADE
-)
+);
 CREATE TABLE MANAGER(
 manager_id int (12) NOT NULL,
 manager_name varchar(30),
 PRIMARY KEY(manager_id)
-)
+);
 
 CREATE TABLE EMPLOYEES(
 employee_id int(5) NOT NULL,
@@ -59,14 +59,14 @@ CONSTRAINT FK_manager_id2 FOREIGN KEY(manager_id) REFERENCES MANAGER(manager_id)
 ON UPDATE CASCADE,
 CONSTRAINT FK_job_id FOREIGN KEY(job_id) REFERENCES JOBS(job_id) 
 ON UPDATE CASCADE
-)
+);
 CREATE TABLE JOBS(
 job_id int(5) NOT NULL,
 job_title varchar(30) NOT NULL,
 min_salary int,
 max_salary int,
 PRIMARY KEY (job_id)
-)
+);
 CREATE TABLE JOB_HISTORY(
 employee_id int(5) NOT NULL,
 start_date DATE NOT NULL,
@@ -78,4 +78,4 @@ CONSTRAINT FK_departament_id2 FOREIGN KEY(departament_id) REFERENCES DEPARTAMENT
 ON UPDATE CASCADE,
 CONSTRAINT FK_job_id2 FOREIGN KEY(job_id) REFERENCES JOBS(job_id) 
 ON UPDATE CASCADE
-)
+);
